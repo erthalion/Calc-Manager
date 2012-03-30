e5f1ed40fe5692bb4cdd3f70908cc32ab7e5b4e6
erthalion <9erthalion6@gmail.com>
2012-03-30 21:17:53 +0700

    Add logging and override some methods

--- TaskServer.py
+++ TaskServer.py
@@ -5,8 +5,25 @@ import SocketServer
 import threading
 import logging
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(name)s: %(message)s',
+                   )
+
 class TaskRequestHandler( SocketServer.BaseRequestHandler):
+
+    def __init__(self,request,client_address,server):
+        self.logger = logging.getLogger('TaskRequestHandler')
+        self.logger.debug('__init__')
+        SocketServer.BaseRequestHandler.__init__(self,request,client_address,server)
+        return
+
+    def setup(self):
+        self.logger.debug('setup')
+        return SocketServer.BaseRequestHandler.setup(self)
+
     def handle(self):
+        self.logger.debug('handle')
+
         data = self.request.recv(1024)
         print data
         data = data.replace('\n','')
@@ -24,19 +41,63 @@ class TaskRequestHandler( SocketServer.BaseRequestHandler):
             response = 'echo'
             self.request.sendall(response)
 
+    def finish(self):
+        self.logger.debug('finish')
+        return SocketServer.BaseRequestHandler.finish(self)
+
 
 class TaskServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     allow_reuse_address = 1
 
+    def  __init__(self, server_address, handler_class):
+        self.logger = logging.getLogger('TaskServer')
+        self.logger.debug('__init__')
+        SocketServer.TCPServer.__init__(self, server_address, handler_class)
+        return
 
-def main():
+    def server_activate(self):
+        self.logger.debug('server_activate')
+        SocketServer.TCPServer.server_activate(self)
+        return
+
+    #def serve_forever(self):
+    #    self.logger.debug('waiting for request')
+    #    self.logger.info('Handling requests, press <Ctrl-C> to quit')
+    #    while True:
+    #        self.handle_request()
+    #        return
+
+    def handle_request(self):
+        self.logger.debug('handle_request')
+        return SocketServer.TCPServer.handle_request(self)
+
+    def verify_request(self, request, client_address):
+        self.logger.debug('verify_request(%s, %s)', request, client_address)
+        return SocketServer.TCPServer.verify_request(self, request, client_address)
+
+    #def process_request(self, request, client_address):
+    #    self.logger.debug('process_request(%s, %s)', request, client_address)
+    #    return SocketServer.TCPServer.process_request(self, request, client_address)
+
+    def server_close(self):
+        self.logger.debug('server_close')
+        return SocketServer.TCPServer.server_close(self)
+
+    def finish_request(self, request, client_address):
+        self.logger.debug('finish_request(%s, %s)', request, client_address)
+        return SocketServer.TCPServer.finish_request(self, request, client_address)
+
+    def close_request(self, request_address):
+        self.logger.debug('close_request(%s)', request_address)
+        return SocketServer.TCPServer.close_request(self, request_address)
+
+def main():     
     HOST='localhost'
     PORT=9997
     server = TaskServer((HOST,PORT),TaskRequestHandler)
     server_thread = threading.Thread(target=server.serve_forever)
     #server_thread.daemon = True
     server_thread.start()
-    logging.debug("server started!")
 
 
 if __name__ == "__main__":
